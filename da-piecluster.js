6f77016e30a43d72040251d5c06f3b24cd9a3241
Rodrigo Derteano <rodrigoderteano@gmail.com>
2014-09-14 11:22:28 -0400

    debug

--- da-piecluster.js
+++ da-piecluster.js
@@ -53,7 +53,7 @@
 			arcs = vis.selectAll( 'g.slice' ).data( pie ).enter().append( 'svg:g' ).attr( 'class', 'slice' );
 			arcs.append( 'svg:path' )
 			.attr( 'fill', function ( a ){
-				if ( ( campaign && campaign.secondary_tags.length ) || ( tags && tags.length ) === 0 ) {
+				if ( ( campaign && !campaign.secondary_tags.length ) || ( tags && tags.length ) === 0 ) {
 					return pieclusterConfig.defaultColor;
 				} else if ( a.data.tag === 'Otros' ) {
 					return pieclusterConfig.defaultColor1;
